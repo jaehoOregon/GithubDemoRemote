9727aa13d770b283e877fd7f56b0030f69ffdc8a
JaehoBuildiOSApp <jaeho.jung.01@gmail.com>
2022-11-03 19:26:18 +0900

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Jaeho Jung on 2022/11/03.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
